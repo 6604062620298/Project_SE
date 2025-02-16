e14eaa33e83eccee011f3c9e0c986bbc1ed083ee
MEASWs <mookvarin.cc1976@gmail.com>
2025-02-16 21:11:25 +0700

    เปลี่ยนนามสกุล

--- app/Payment/page.jsx
+++ app/Payment/page.jsx
@@ -11,7 +11,7 @@ function page() {
         <div className="card bg-base-content text-primary-content shadow-lg">
           <div className="card-body text-base-100">
             <h2 className="card-title text-3xl mb-8">Sigma Boy</h2>
-            <p>ชื่อ: นาย ศรสรัน พลแสน</p>
+            <p>ชื่อ: นาย ศรสรัน บุษษะ</p>
             <p>ห้อง: 206</p>
             <p>ค่าน้ำประปา: 30</p>
             <p>ค่าไฟฟ้า: 1000</p>
